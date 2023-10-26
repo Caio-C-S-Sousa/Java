aa47d7bf91479a8f510c25004ad66c36d12fe3d2
Caio Sousa <caiyo.projeto@gmail.com>
2023-10-26 20:32:44 -0300

    Entity nos modelos e Id do produto.

--- appvenda/appvenda/src/main/java/br/edu/infnet/appvenda/model/domain/Produto.java
+++ appvenda/appvenda/src/main/java/br/edu/infnet/appvenda/model/domain/Produto.java
@@ -1,6 +1,19 @@
 package br.edu.infnet.appvenda.model.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TProduto")
 public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
 	private String descricao;
 	
 	private int codigo;
@@ -11,7 +24,15 @@ public class Produto {
 
 	@Override
 	public String toString() {
-		return String.format("%d - %s - %.2f - %s", codigo , descricao, preco, estoque);
+		return String.format("%d -  %d - %s - %.2f - %s", id, codigo, descricao, preco, estoque);
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 	
 	public String getDescricao() {
--- appvenda/appvenda/src/main/webapp/WEB-INF/jsp/index.jsp
+++ appvenda/appvenda/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,5 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,6 +7,5 @@
 </head>
 <body>
 	<h1>Hello World</h1>
-	<h2>Link Repositorio: </h2>
 </body>
 </html>
